a9bc2ea3d43ca04d46ccb52a8d41e773748ad61b
Oriol Segura Nilo <okiko22@gmail.com>
2023-03-11 13:54:09 +0100

    Social media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,12 @@ class Footer extends Component {
 	render() {
 		return (
 			<footer className="page-footer">
-				<p>
-					Page footer
-				</p>
+                <div className="social-media">
+                    <a href="https://twitter.com/arreplegats" className="icon" target="_blank"><img src="font-awesome/twitter.svg" alt="" /></a>
+                    <a href="https://www.instagram.com/arreplegats/" className="icon" target="_blank"><img src="font-awesome/instagram.svg" alt="" /></a>
+                    <a href="https://www.youtube.com/channel/UC-RVCefwipBS8WutREwbTmw/videos" className="icon" target="_blank"><img src="font-awesome/youtube.svg" alt="" /></a>
+                    <a href="https://www.tiktok.com/@arreplegats" className="icon" target="_blank"><img src="font-awesome/tiktok.svg" alt="" /></a>
+                </div>
 			</footer>
 		);
 	}
