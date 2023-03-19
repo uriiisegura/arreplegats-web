75b47da2f71fa68fb7840484c90aa2f7e5cd657f
Oriol Segura Nilo <okiko22@gmail.com>
2023-03-19 16:37:00 +0100

    Improve BarraLliure

--- src/pages/BarraLliure.js
+++ src/pages/BarraLliure.js
@@ -22,39 +22,43 @@ class BarraLliure extends Component {
 	componentDidMount() {
 	}
 	render() {
-		let codeOne = this.getCookie('codeOne');
-		let codeTwo = this.getCookie('codeTwo');
-		let codeThree = this.getCookie('codeThree');
-		if (codeOne === null || codeTwo === null || codeThree === null) {
-			codeOne = Math.floor(Math.random() * 10000000000000);
-			codeTwo = Math.floor(Math.random() * 10000000000000);
-			codeThree = Math.floor(Math.random() * 10000000000000);
-			document.cookie = 'codeOne='+codeOne;
-			document.cookie = 'codeTwo='+codeTwo;
-			document.cookie = 'codeThree='+codeThree;
+		let code = this.getCookie('code');
+		let secret_p1 = this.getCookie('secret_p1');
+		let secret_p2 = this.getCookie('secret_p2');
+		if (code === null || secret_p1 === null || secret_p2 === null) {
+			code = Math.floor(Math.random() * 10000000000000);
+			secret_p1 = Math.floor(Math.random() * 10000000000000);
+			secret_p2 = Math.floor(Math.random() * 10000000000000);
+			document.cookie = 'code='+code;
+			document.cookie = 'secret_p1='+secret_p1;
+			document.cookie = 'secret_p2='+secret_p2;
 		}
 
 		return (<>
 			<section>
+				<div className="confirmation">
+					<img src="confirmation.png" alt="Confirmation" />
+					<h3>S'ha generat un codi únic per a que puguis accedir a la barra lliure durant l'animació de la <span>XXVIII Diada dels Arreplegats de la Zona Universitària</span>, gaudeix de la festa!</h3>
+				</div>
 				<div className="barcode-wrap">
 					<div className="barcode">
 						{
-							String(codeOne).split('').map(d => {
+							String(code).split('').map(d => {
 								return <div className="bar" style={{width: `${parseInt(d)+1}px`}}></div>;
 							})
 						}
 						{
-							String(codeTwo).split('').map(d => {
+							String(secret_p1).split('').map(d => {
 								return <div className="bar" style={{width: `${parseInt(d)+1}px`}}></div>;
 							})
 						}
 						{
-							String(codeThree).split('').map(d => {
+							String(secret_p2).split('').map(d => {
 								return <div className="bar" style={{width: `${parseInt(d)+1}px`}}></div>;
 							})
 						}
 					</div>
-					<p>{codeOne}</p>
+					<p>{code}</p>
 				</div>
 			</section>
 		</>);
