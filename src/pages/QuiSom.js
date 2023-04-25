58cc680b161d600da437fd081b8e326100e939ba
Andreu Huguet <andreuhuguet@gmail.com>
2023-04-26 01:34:48 +0200

    Update QuiSom.js

--- src/pages/QuiSom.js
+++ src/pages/QuiSom.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./ImageResizes.css"
 
 class QuiSom extends Component {
 	render() {
@@ -12,7 +13,7 @@ class QuiSom extends Component {
 					<p><span>adj. i m. i f.</span> [LC] Considerat incopenent per a una feina concreta. <i>Una colla d'arreplegats.</i></p>
 				</div>
 			</section>
-			<section className="image-divider" style={{backgroundImage: `url('images/3d8f-cors.jpg')`}}></section>
+			<section className="image-divider qui-som"></section>
 			<section>
 				<h3>Som els <span>Arreplegats</span> de la Zona Universitària</h3>
 
