e89affa6371bc096b38fb181037a784e1adc2ca9
Oriol Segura Nilo <okiko22@gmail.com>
2023-05-07 17:21:01 +0200

    Fix typos

--- src/pages/HistoriaDeLaColla.js
+++ src/pages/HistoriaDeLaColla.js
@@ -15,7 +15,7 @@ class HistoriaDeLaColla extends Component {
 					El 1997 es va fer el primer viatge a Perpinyà, on finalment vam descarregar el primer 3 de 6 de la colla. Era un bon presagi, perquè l'objectiu de la temporada era el 5 de 6, que vam descarregar a la Diada el maig d'aquell mateix any, junt amb el primer pilar de 5 carregat.
 				</p>
 				<p>
-					La temporada següent, vam experimentar un avenç brutal, carregant i després descarregant la primera torre de 6, descarregant el tres de 6 per sota, i acabant l'èxtasi amb el primer 4 de 7 carregat. Provar un castell de 7 pisos era un salt al desconegut, una nova dimensió en el món casteller universitari. Aquell any també es va descarregar el primer pilar de 5, i es va aconseguir carrer del 3 de 6 net carregat, que obria les portes al 3 de 7.
+					La temporada següent, vam experimentar un avenç brutal, carregant i després descarregant la primera torre de 6, descarregant el tres de 6 per sota, i acabant l'èxtasi amb el primer 4 de 7 carregat. Provar un castell de 7 pisos era un salt al desconegut, una nova dimensió en el món casteller universitari. Aquell any també es va descarregar el primer pilar de 5, i es va aconseguir carregar del 3 de 6 net carregat, que obria les portes al 3 de 7.
 				</p>
 				<p>
 					Aquest castell, el 3 de 7, es va fer esperar més, sent el primer descarregat al final de la temporada 2000-2001. El següent any, es va carregar el primer 4 de 7 amb l'agulla, descarregat un any després. La temporada 2003-2004 va acabar amb dos nous castells: el <NavLink to="/castells/5d7">5 de 7</NavLink> i la torre de 7 amb folre, ambdós descarregats. El següent castell amb folre va arribar durant la temporada 2006-2007, quan vam carregar el primer pilar de 6 amb folre de la història castellera universitària. Aquest castell finalment es va descarregar a les acaballes de la temporada 2007-2008, en la Diada de Primavera dels Ganàpies de l'Autònoma.
@@ -51,7 +51,7 @@ class HistoriaDeLaColla extends Component {
 					La següent temporada els Arreplegats vam seguir escribint la història dels castells. El maig de 2015, en la nostra 20a Diada, vam descarregar el primer <NavLink to="/castells/9d7">9 de 7</NavLink> universitari que mai ningú ha tornat a intentar.
 				</p>
 				<p>
-					Aquests anys vam dominar tots els grans castells. Podiem fer, amb relativa facilitat, els castells més complicats del món casteller universitari, castells que ningú ha pogut igualar encara. Vam passejar per tot arreu els ja esmentats <NavLink to="/castells/3d8f">3 de 8 amb folre</NavLink>, <NavLink to="/castells/3d7s">3 de 7 per sota</NavLink>, <NavLink to="/castells/5d7">5 de 7</NavLink>, i <NavLink to="/castells/4d8">4 de 8</NavLink> demostrant per totes les places on passàvem en nostre domini.
+					Aquests anys vam dominar tots els grans castells. Podiem fer, amb relativa facilitat, els castells més complicats del món casteller universitari, castells que ningú ha pogut igualar encara. Vam passejar per tot arreu els ja esmentats <NavLink to="/castells/3d8f">3 de 8 amb folre</NavLink>, <NavLink to="/castells/3d7s">3 de 7 per sota</NavLink>, <NavLink to="/castells/5d7">5 de 7</NavLink>, i <NavLink to="/castells/4d8">4 de 8</NavLink> demostrant el nostre domini per totes les places on passàvem.
 				</p>
 				<p>
 					Aquesta època gloriosa va assolir el seu punt més àlgid el 5 de maig del 2016, on vam juntar el <NavLink to="/castells/3d8f">3 de 8 amb folre</NavLink>, el <NavLink to="/castells/4d8">4 de 8</NavLink>, i vam carregar la inèdita <NavLink to="/castells/Td8fm">torre de 8 amb folre i manilles</NavLink> en una diada històrica.
@@ -66,7 +66,7 @@ class HistoriaDeLaColla extends Component {
 			<section className="historia">
 				<h5>Represa després de la COVID-19</h5>
 				<p>
-					En reprendre l'activitat castellera després del confinament, hi havia folta feina a fer. Renovació, formació, recaptació de nous castellers, etc. Va ser un procès llarg però no per això el vam encarar amb menys il·lusió.
+					En reprendre l'activitat castellera després del confinament, hi havia molta feina a fer. Renovació, formació, recaptació de nous castellers, etc. Va ser un procès llarg però no per això el vam encarar amb menys il·lusió.
 				</p>
 				<p>
 					La diada més important des d'aleshores ha estat el nostre 27è Aniversari, el 22 de desembre de 2022. Després d'una gran temporada amb molta formació, vam poder conrear els fruits de tota la feina feta, i vam fer-hi el primer <NavLink to="/castells/5d7">5 de 7</NavLink> universitari post-COVID, el primer <NavLink to="/castells/3d8f">3 de 8 amb folre</NavLink> universitari post-COVID, la nostra quarta torre de 7 amb folre de la temporada, i hi vam carregar el <NavLink to="/castells/Pd7fm">pilar de 7 amb folre i manilles</NavLink>. Aquest pilar no només representava les primeres manilles universitàries post-COVID, sinó que també ens va convertir en la sisena colla global en fer unes manilles després de la pandèmia.
