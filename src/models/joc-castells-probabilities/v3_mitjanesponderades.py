7b0fe79a2b644149dfacf742d2551b640c7e0c25
Oriol Segura Nilo <okiko22@gmail.com>
2023-08-01 01:18:38 +0200

    Update v3_mitjanesponderades.py

--- src/models/joc-castells-probabilities/v3_mitjanesponderades.py
+++ src/models/joc-castells-probabilities/v3_mitjanesponderades.py
@@ -1,168 +1,195 @@
+import numpy as np
+from random import choices
+from os import system
+
+
+RESULTS = ['D', 'C', 'I', 'ID']
+
 CASTELLS = {
-    "pd3": {
+    "Pd3": {
         "pes_dependencies": 0,
         "unique": [0.05, 0.15, 0.4, 0.3],
         "dependencies": {}
     },
-    "pd3s": {
+    "Pd3s": {
         "pes_dependencies": 0.6,
         "unique": [0, 0.1, 0.8, 0.1],
         "dependencies": {
-            "pd3": 1
+            "Pd3": 1
         }
     },
-    "pd3n": {
+    "Pd3n": {
         "pes_dependencies": 0.15,
         "unique": [0, 0.1, 0.8, 0.1],
         "dependencies": {
-            "pd3": 1
+            "Pd3": 1
         }
     },
-    "pd4": {
+    "Pd4": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.8, 0.2],
         "dependencies": {
-            "pd3n": 0.8,
-            "pd3": 0.2
+            "Pd3n": 0.8,
+            "Pd3": 0.2
         }
     },
-    "pd4s": {
+    "Pd4s": {
         "pes_dependencies": 0.6,
         "unique": [0, 0, 0.8, 0.2],
         "dependencies": {
-            "pd3n": 0.8,
-            "pd3s": 0.2
+            "Pd3n": 0.8,
+            "Pd3s": 0.2
         }
     },
-    "pd4n": {
+    "Pd4n": {
         "pes_dependencies": 0.15,
         "unique": [0, 0, 0.8, 0.2],
         "dependencies": {
-            "pd3n": 1,
+            "Pd3n": 1,
         }
     },
-    "pd5": {
+    "Pd5": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "pd4n": 0.8,
-            "pd4": 0.2
+            "Pd4n": 0.8,
+            "Pd4": 0.2
         }
     },
-    "pd5s": {
+    "Pd5s": {
         "pes_dependencies": 0.4,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "pd4n": 0.8,
-            "pd4s": 0.2
+            "Pd4n": 0.8,
+            "Pd4s": 0.2
         }
     },
-    "pd5n": {
+    "Pd5n": {
         "pes_dependencies": 0.05,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "pd4n": 1,
+            "Pd4n": 1,
         }
     },
-    "pd6sf": {
+    "Pd6sf": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "pd5n": 0.95,
-            "pd5": 0.05
+            "Pd5n": 0.95,
+            "Pd5": 0.05
         }
     },
-    "ftpd6f": {
+    "FT Pd6f": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "pd5": 1
+            "Pd5": 1
         }
     },
-    "pd6f": {
+    "Pd6f": {
         "pes_dependencies": 0.3,
         "unique": [0, 0, 0.05, 0.95],
         "dependencies": {
-            "ftpd6f": 0.8
+            "FT Pd6f": 0.8
         }
     },
-    "mtpd7fm": {
+    "MT Pd7fm": {
         "pes_dependencies": 0.9,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "ftpd6f": 1,
+            "FT Pd6f": 1,
         }
     },
-    "ftpd7fm": {
+    "FT Pd7fm": {
         "pes_dependencies": 0.7,
         "unique": [0, 0, 0.05, 0.95],
         "dependencies": {
-            "pd6f": 0.8,
-            "mtpd7fm": 0.2
+            "Pd6f": 0.8,
+            "MT Pd7fm": 0.2
         }
     },
-    "pd7fm": {
+    "Pd7fm": {
         "pes_dependencies": 0.2,
         "unique": [0, 0, 0, 1],
         "dependencies": {
-            "ftpd7fm": 1
+            "FT Pd7fm": 1
         }
     },
-    "ptpd8fmp": {
+    "PT Pd8fmp": {
         "pes_dependencies": 0.7,
         "unique": [0, 0, 0.2, 0.8],
         "dependencies": {
-            "mtpd7fm": 1
+            "MT Pd7fm": 1
         }
     },
-    "mtftpd8fmp": {
+    "MT Pd8fmp": {
         "pes_dependencies": 0.7,
         "unique": [0, 0, 0.05, 0.95],
         "dependencies": {
-            "ftpd7fm": 0.8,
-            "ptpd8fmp": 0.2
+            "FT Pd7fm": 0.8,
+            "PT Pd8fmp": 0.2
         }
     },
-    "ftpd8fmp": {
+    "FT Pd8fmp": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0, 1],
         "dependencies": {
-            "pd7fm": 0.8,
-            "mtftpd8fmp": 0.2
+            "Pd7fm": 0.8,
+            "MT Pd8fmp": 0.2
         }
     },
-    "pd8fmp": {
+    "Pd8fmp": {
         "pes_dependencies": 0.3,
         "unique": [0, 0, 0, 1],
         "dependencies": {
-            "ftpd8fmp": 0.7,
-            "mtftpd8fmp": 0.2,
-            "ptpd8fmp": 0.1
+            "FT Pd8fmp": 0.7,
+            "MT Pd8fmp": 0.2,
+            "PT Pd8fmp": 0.1
         }
     }
 }
 
-def PFinal(castell):
-    import numpy as np
 
+def final_probability(castell):
     probs = CASTELLS[castell]
-    PD = probs["pes_dependencies"]
-    U = probs["unique"]
-    D = probs["dependencies"]
+    beta = probs['pes_dependencies']
+    U = probs['unique']
+    D = probs['dependencies']
 
-    deps = np.array([ k*PFinal(d) for d, k in D.items() ])
+    dependencies = np.array([final_probability(d) * k for d, k in D.items()])
     unique = np.array(U)
 
-    if len(deps) == 0:
+    if len(dependencies) == 0:
         return unique
     else:
-        return PD*np.sum(deps) + (1-PD)*unique
+        return beta * np.sum(dependencies) + (1 - beta) * unique
+
 
 def simulate_play(castell):
-    import random
+    probs = final_probability(castell)
+    return choices(RESULTS, probs)[0]
 
-    probs = PFinal(castell)
-    return random.choices(["D", "C", "I", "ID"], probs)[0]
 
-for i in range(10):
-    print(simulate_play("pd4"))
+def main(castell, n=10):
+    total = {k: 0 for k in RESULTS}
+
+    for _ in range(n):
+        res = simulate_play(castell)
+        total[res] += 1
+    
+    print(
+        f'Simulation for {castell}',
+        f'N = {n}',
+        f'====================',
+        f'Nombre de  D: {total["D"]}',
+        f'Nombre de  C: {total["C"]}',
+        f'Nombre de  I: {total["I"]}',
+        f'Nombre de ID: {total["ID"]}',
+        sep='\n'
+    )
+
+
+if __name__ == '__main__':
+    system('clear')
+
+    main('Pd3', n=1000)
