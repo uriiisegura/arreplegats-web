20a1171cd9d11536eddeef1149f47f29a7f3b172
Oriol Segura Nilo <okiko22@gmail.com>
2023-08-01 01:52:34 +0200

    Update v3_mitjanesponderades.py

--- src/models/joc-castells-probabilities/v3_mitjanesponderades.py
+++ src/models/joc-castells-probabilities/v3_mitjanesponderades.py
@@ -5,22 +5,29 @@ from os import system
 
 RESULTS = ['D', 'C', 'I', 'ID']
 
+EFFECTS = {
+    'D':  [ 1.10, 0.90, 0.85, 1.00 ],
+    'C':  [ 1.05, 1.05, 0.90, 1.05 ],
+    'I':  [ 0.95, 1.05, 0.80, 1.10 ],
+    'ID': [ 1.05, 1.00, 1.05, 0.80 ]
+}
+
 CASTELLS = {
     "Pd3": {
         "pes_dependencies": 0,
-        "unique": [0.05, 0.15, 0.4, 0.3],
+        "unique": [0.15, 0.25, 0.6, 0],
         "dependencies": {}
     },
     "Pd3s": {
         "pes_dependencies": 0.6,
-        "unique": [0, 0.1, 0.8, 0.1],
+        "unique": [0, 0.1, 0.9, 0],
         "dependencies": {
             "Pd3": 1
         }
     },
     "Pd3n": {
         "pes_dependencies": 0.15,
-        "unique": [0, 0.1, 0.8, 0.1],
+        "unique": [0, 0.15, 0.85, 0],
         "dependencies": {
             "Pd3": 1
         }
@@ -162,34 +169,68 @@ def final_probability(castell):
     if len(dependencies) == 0:
         return unique
     else:
-        return beta * np.sum(dependencies) + (1 - beta) * unique
+        return beta * np.sum(dependencies, axis=0) + (1 - beta) * unique
+
+
+def improve_unique(result, castell):
+    probs = CASTELLS[castell]
+    npU = np.array(probs['unique'])
+
+    npU *= EFFECTS[result]
+
+    npU[0] = min(npU[0], 0.95)
+    npU[1] = min(npU[1], 0.95)
+    npU[2] = max(0.05, min(npU[2], 0.95))
+    npU[3] = max(0.05, min(npU[3], 0.95))
+
+    if np.sum(npU) != 0:
+        npU /= np.sum(npU)
+    else:
+        npU = np.array([0, 0, 0, 1])
+    
+    probs['unique'] = npU.tolist()
+
+    return npU
 
 
 def simulate_play(castell):
     probs = final_probability(castell)
-    return choices(RESULTS, probs)[0]
+    result = choices(RESULTS, probs)[0]
+    improve_unique(result, castell)
+    return result
 
 
-def main(castell, n=10):
+def main(castell, n=100, must_print=True):
     total = {k: 0 for k in RESULTS}
+
+    if must_print:
+        print(
+            f'Simulation for {castell}',
+            f'N = {n}',
+            f'=====================',
+            f'Initial probs:',
+            final_probability(castell).tolist(),
+            sep='\n'
+        )
 
     for _ in range(n):
         res = simulate_play(castell)
         total[res] += 1
     
-    print(
-        f'Simulation for {castell}',
-        f'N = {n}',
-        f'====================',
-        f'Nombre de  D: {total["D"]}',
-        f'Nombre de  C: {total["C"]}',
-        f'Nombre de  I: {total["I"]}',
-        f'Nombre de ID: {total["ID"]}',
-        sep='\n'
-    )
+    if must_print:
+        print(
+            f'Final probs:',
+            final_probability(castell).tolist(),
+            f'=====================',
+            f'Nombre de  D: {total["D"]}',
+            f'Nombre de  C: {total["C"]}',
+            f'Nombre de  I: {total["I"]}',
+            f'Nombre de ID: {total["ID"]}',
+            sep='\n'
+        )
 
 
 if __name__ == '__main__':
     system('clear')
 
-    main('Pd3', n=1000)
+    main('Pd3', n=20)
