c4f5cfc3532d8dc99ab4a0274e1a8e268eb51232
Andreu <andreuhuguet@gmail.com>
2023-08-01 00:46:21 +0200

    Probabilitats propostes

---
+++ src/models/joc-castells-probabilities/v3_mitjanesponderades.py
@@ -0,0 +1,143 @@
+CASTELLS = {
+    "pd3": {
+        "pes_dependencies": 0,
+        "unique": [0.05, 0.15, 0.4, 0.3],
+        "dependencies": {}
+    },
+    "pd3s": {
+        "pes_dependencies": 0.6,
+        "unique": [0, 0.1, 0.8, 0.1],
+        "dependencies": {
+            "pd3": 1
+        }
+    },
+    "pd3n": {
+        "pes_dependencies": 0.15,
+        "unique": [0, 0.1, 0.8, 0.1],
+        "dependencies": {
+            "pd3": 1
+        }
+    },
+    "pd4": {
+        "pes_dependencies": 0.8,
+        "unique": [0, 0, 0.8, 0.2],
+        "dependencies": {
+            "pd3n": 0.8,
+            "pd3": 0.2
+        }
+    },
+    "pd4s": {
+        "pes_dependencies": 0.6,
+        "unique": [0, 0, 0.8, 0.2],
+        "dependencies": {
+            "pd3n": 0.8,
+            "pd3s": 0.2
+        }
+    },
+    "pd4n": {
+        "pes_dependencies": 0.15,
+        "unique": [0, 0, 0.8, 0.2],
+        "dependencies": {
+            "pd3n": 1,
+        }
+    },
+    "pd5": {
+        "pes_dependencies": 0.8,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "pd4n": 0.8,
+            "pd4": 0.2
+        }
+    },
+    "pd5s": {
+        "pes_dependencies": 0.4,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "pd4n": 0.8,
+            "pd4s": 0.2
+        }
+    },
+    "pd5n": {
+        "pes_dependencies": 0.05,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "pd4n": 1,
+        }
+    },
+    "pd6sf": {
+        "pes_dependencies": 0.8,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "pd5n": 0.95,
+            "pd5": 0.05
+        }
+    },
+    "ftpd6f": {
+        "pes_dependencies": 0.8,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "pd5": 1
+        }
+    },
+    "pd6f": {
+        "pes_dependencies": 0.3,
+        "unique": [0, 0, 0.05, 0.95],
+        "dependencies": {
+            "ftpd6f": 0.8
+        }
+    },
+    "mtpd7fm": {
+        "pes_dependencies": 0.9,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "ftpd6f": 1,
+        }
+    },
+    "ftpd7fm": {
+        "pes_dependencies": 0.7,
+        "unique": [0, 0, 0.05, 0.95],
+        "dependencies": {
+            "pd6f": 0.8,
+            "mtpd7fm": 0.2
+        }
+    },
+    "pd7fm": {
+        "pes_dependencies": 0.2,
+        "unique": [0, 0, 0, 1],
+        "dependencies": {
+            "ftpd7fm": 1
+        }
+    },
+    "ptpd8fmp": {
+        "pes_dependencies": 0.7,
+        "unique": [0, 0, 0.2, 0.8],
+        "dependencies": {
+            "mtpd7fm": 1
+        }
+    },
+    "mtftpd8fmp": {
+        "pes_dependencies": 0.7,
+        "unique": [0, 0, 0.05, 0.95],
+        "dependencies": {
+            "ftpd7fm": 0.8,
+            "ptpd8fmp": 0.2
+        }
+    },
+    "ftpd8fmp": {
+        "pes_dependencies": 0.8,
+        "unique": [0, 0, 0, 1],
+        "dependencies": {
+            "pd7fm": 0.8,
+            "mtftpd8fmp": 0.2
+        }
+    },
+    "pd8fmp": {
+        "pes_dependencies": 0.3,
+        "unique": [0, 0, 0, 1],
+        "dependencies": {
+            "ftpd8fmp": 0.7,
+            "mtftpd8fmp": 0.2,
+            "ptpd8fmp": 0.1
+        }
+    }
+}
