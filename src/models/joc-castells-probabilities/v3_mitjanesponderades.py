3b2e4b9db00027307bc0a639da914a9e3302eceb
Oriol Segura Nilo <okiko22@gmail.com>
2023-08-01 11:52:27 +0200

    Update v3_mitjanesponderades.py

--- src/models/joc-castells-probabilities/v3_mitjanesponderades.py
+++ src/models/joc-castells-probabilities/v3_mitjanesponderades.py
@@ -1,26 +1,31 @@
 import numpy as np
 from random import choices
+from copy import deepcopy
 from os import system
 
 
 RESULTS = ['D', 'C', 'I', 'ID']
 
 EFFECTS = {
-    'D':  [ 1.20, 0.85, 0.85, 0.85 ],
-    'C':  [ 1.10, 1.05, 0.90, 1.05 ],
-    'I':  [ 0.95, 1.05, 0.80, 1.10 ],
-    'ID': [ 1.00, 1.00, 1.05, 0.80 ]
+    'D':  [ +0.75, -0.20, -0.80, +0.25 ],
+    'C':  [ +0.65, +0.35, -0.60, -0.40 ],
+    'I':  [ -0.65, +0.40, -0.35, +0.60 ],
+    'ID': [ +0.30, -0.30, +0.70, -0.70 ]
 }
 
 CASTELLS = {
     "Pd3": {
         "pes_dependencies": 0,
         "unique": [0.15, 0.25, 0.6, 0],
+        "max": 0.99,
+        "k": 0.12,
         "dependencies": {}
     },
     "Pd3s": {
         "pes_dependencies": 0.6,
         "unique": [0, 0.1, 0.9, 0],
+        "max": 0.99,
+        "k": 0.15,
         "dependencies": {
             "Pd3": 1
         }
@@ -28,6 +33,8 @@ CASTELLS = {
     "Pd3n": {
         "pes_dependencies": 0.15,
         "unique": [0, 0.15, 0.85, 0],
+        "max": 0.99,
+        "k": 0.1,
         "dependencies": {
             "Pd3": 1
         }
@@ -35,6 +42,8 @@ CASTELLS = {
     "Pd4": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.8, 0.2],
+        "max": 0.99,
+        "k": 0.1,
         "dependencies": {
             "Pd3n": 0.8,
             "Pd3": 0.2
@@ -43,6 +52,8 @@ CASTELLS = {
     "Pd4s": {
         "pes_dependencies": 0.6,
         "unique": [0, 0, 0.8, 0.2],
+        "max": 0.99,
+        "k": 0.1,
         "dependencies": {
             "Pd3n": 0.8,
             "Pd3s": 0.2
@@ -51,6 +62,8 @@ CASTELLS = {
     "Pd4n": {
         "pes_dependencies": 0.15,
         "unique": [0, 0, 0.8, 0.2],
+        "max": 0.98,
+        "k": 0.09,
         "dependencies": {
             "Pd3n": 1,
         }
@@ -58,6 +71,8 @@ CASTELLS = {
     "Pd5": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.2, 0.8],
+        "max": 0.99,
+        "k": 0.08,
         "dependencies": {
             "Pd4n": 0.8,
             "Pd4": 0.2
@@ -66,29 +81,18 @@ CASTELLS = {
     "Pd5s": {
         "pes_dependencies": 0.4,
         "unique": [0, 0, 0.2, 0.8],
+        "max": 0.85,
+        "k": 0.02,
         "dependencies": {
             "Pd4n": 0.8,
             "Pd4s": 0.2
         }
     },
-    "Pd5n": {
-        "pes_dependencies": 0.05,
-        "unique": [0, 0, 0.2, 0.8],
-        "dependencies": {
-            "Pd4n": 1,
-        }
-    },
-    "Pd6sf": {
-        "pes_dependencies": 0.8,
-        "unique": [0, 0, 0.2, 0.8],
-        "dependencies": {
-            "Pd5n": 0.95,
-            "Pd5": 0.05
-        }
-    },
     "FT Pd6f": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0.2, 0.8],
+        "max": 0.99,
+        "k": 0.07,
         "dependencies": {
             "Pd5": 1
         }
@@ -96,6 +100,8 @@ CASTELLS = {
     "Pd6f": {
         "pes_dependencies": 0.3,
         "unique": [0, 0, 0.05, 0.95],
+        "max": 0.97,
+        "k": 0.05,
         "dependencies": {
             "FT Pd6f": 0.8
         }
@@ -103,6 +109,8 @@ CASTELLS = {
     "MT Pd7fm": {
         "pes_dependencies": 0.9,
         "unique": [0, 0, 0.2, 0.8],
+        "max": 0.99,
+        "k": 0.065,
         "dependencies": {
             "FT Pd6f": 1,
         }
@@ -110,6 +118,8 @@ CASTELLS = {
     "FT Pd7fm": {
         "pes_dependencies": 0.7,
         "unique": [0, 0, 0.05, 0.95],
+        "max": 0.97,
+        "k": 0.045,
         "dependencies": {
             "Pd6f": 0.8,
             "MT Pd7fm": 0.2
@@ -118,13 +128,37 @@ CASTELLS = {
     "Pd7fm": {
         "pes_dependencies": 0.2,
         "unique": [0, 0, 0, 1],
+        "max": 0.80,
+        "k": 0.009,
         "dependencies": {
-            "FT Pd7fm": 1
+            "MT Pd7fm": 0.15,
+            "FT Pd7fm": 0.85
+        }
+    },
+    "Pd5n": {
+        "pes_dependencies": 0.05,
+        "unique": [0, 0, 0.2, 0.8],
+        "max": 0.60,
+        "k": 0.01,
+        "dependencies": {
+            "Pd4n": 1,
+        }
+    },
+    "Pd6sf": {
+        "pes_dependencies": 0.8,
+        "unique": [0, 0, 0.2, 0.8],
+        "max": 0.65,
+        "k": 0.005,
+        "dependencies": {
+            "Pd5n": 0.95,
+            "Pd5": 0.05
         }
     },
     "PT Pd8fmp": {
         "pes_dependencies": 0.7,
         "unique": [0, 0, 0.2, 0.8],
+        "max": 0.99,
+        "k": 0.06,
         "dependencies": {
             "MT Pd7fm": 1
         }
@@ -132,6 +166,8 @@ CASTELLS = {
     "MT Pd8fmp": {
         "pes_dependencies": 0.7,
         "unique": [0, 0, 0.05, 0.95],
+        "max": 0.98,
+        "k": 0.04,
         "dependencies": {
             "FT Pd7fm": 0.8,
             "PT Pd8fmp": 0.2
@@ -140,6 +176,8 @@ CASTELLS = {
     "FT Pd8fmp": {
         "pes_dependencies": 0.8,
         "unique": [0, 0, 0, 1],
+        "max": 0.95,
+        "k": 0.008,
         "dependencies": {
             "Pd7fm": 0.8,
             "MT Pd8fmp": 0.2
@@ -148,6 +186,8 @@ CASTELLS = {
     "Pd8fmp": {
         "pes_dependencies": 0.3,
         "unique": [0, 0, 0, 1],
+        "max": 0.55,
+        "k": 0.004,
         "dependencies": {
             "FT Pd8fmp": 0.7,
             "MT Pd8fmp": 0.2,
@@ -155,6 +195,8 @@ CASTELLS = {
         }
     }
 }
+
+CASTELLS_CLONE = deepcopy(CASTELLS)
 
 
 def final_probability(castell):
@@ -167,39 +209,42 @@ def final_probability(castell):
     unique = np.array(U)
 
     if len(dependencies) == 0:
-        return unique
+        final = unique
     else:
-        return beta * np.sum(dependencies, axis=0) + (1 - beta) * unique
-
-
-def improve_unique(result, castell):
-    probs = CASTELLS[castell]
-    npU = np.array(probs['unique'])
-
-    npU = np.clip(npU, 0.02, 0.96)
-
-    npU *= EFFECTS[result]
-
-    npU[0] = min(npU[0], 0.95)
-    npU[1] = min(npU[1], 0.95)
-    npU[2] = max(0.05, min(npU[2], 0.95))
-    npU[3] = max(0.05, min(npU[3], 0.95))
-
-    if np.sum(npU) != 0:
-        npU /= np.sum(npU)
-    else:
-        npU = np.array([0, 0, 0, 1])
+        final = beta * np.sum(dependencies, axis=0) + (1 - beta) * unique
     
-    probs['unique'] = npU.tolist()
+    if (diff := final[0] - probs['max']) > 0:
+        final[0] = probs['max']
+        final[1] += diff / 3
+        final[2] += diff / 3
+        final[3] += diff / 3
+    
+    return final
 
-    return npU
+
+def calculate_delta(castell, n):
+    def f(x):
+        k = CASTELLS[castell]['k']
+        return (k * x) / (x - 70 * (1 - k))
+    return f(-n)
 
 
-def simulate_play(castell):
-    probs = final_probability(castell)
-    result = choices(RESULTS, probs)[0]
-    improve_unique(result, castell)
-    return result
+def improve_unique(castell, delta, result):
+    probs = CASTELLS[castell]
+    old_prob = probs['unique']
+    deltas = [k * delta for k in EFFECTS[result]]
+
+    new_prob = []
+    for i in range(4):
+        prob = old_prob[i] + deltas[i]
+        if prob < 0:
+            prob = 0
+        new_prob.append(prob)
+    new_prob = [x / sum(new_prob) for x in new_prob]
+    
+    CASTELLS[castell]['unique'] = new_prob
+
+    return new_prob
 
 
 def main(castell, n=100, must_print=True):
@@ -215,9 +260,19 @@ def main(castell, n=100, must_print=True):
             sep='\n'
         )
 
-    for _ in range(n):
-        res = simulate_play(castell)
-        total[res] += 1
+    for iter in range(n):
+        probs = final_probability(castell)
+        result = choices(RESULTS, probs)[0]
+
+        delta = calculate_delta(castell, sum(total.values()))
+        improve_unique(castell, delta, result)
+
+        if must_print:
+            print(f'===== Iter {iter + 1}/{n} =====')
+            print(f'{result} | delta: {delta}')
+            print(probs.tolist())
+
+        total[result] += 1
     
     if must_print:
         print(
@@ -235,6 +290,87 @@ def main(castell, n=100, must_print=True):
 if __name__ == '__main__':
     system('clear')
 
+    print('======== Pd3 ========')
+    print(f'Initial:\n{final_probability("Pd3").tolist()}')
     main('Pd3', n=100, must_print=False)
-    main('Pd3n', n=1000, must_print=False)
-    main('Pd4', n=100)
+    print(f'Final:\n{final_probability("Pd3").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd3s ========')
+    print(f'Initial:\n{final_probability("Pd3s").tolist()}')
+    main('Pd3', n=100, must_print=False)
+    main('Pd3s', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd3s").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd4 ========')
+    print(f'Initial:\n{final_probability("Pd4").tolist()}')
+    main('Pd3', n=100, must_print=False)
+    main('Pd3n', n=100, must_print=False)
+    main('Pd4', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd4").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd4s ========')
+    print(f'Initial:\n{final_probability("Pd4s").tolist()}')
+    main('Pd3n', n=100, must_print=False)
+    main('Pd3s', n=100, must_print=False)
+    main('Pd4s', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd4s").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd5 ========')
+    print(f'Initial:\n{final_probability("Pd5").tolist()}')
+    main('Pd4', n=100, must_print=False)
+    main('Pd4n', n=100, must_print=False)
+    main('Pd5', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd5").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd5s ========')
+    print(f'Initial:\n{final_probability("Pd5s").tolist()}')
+    main('Pd4n', n=100, must_print=False)
+    main('Pd4s', n=100, must_print=False)
+    main('Pd5s', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd5s").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd6f ========')
+    print(f'Initial:\n{final_probability("Pd6f").tolist()}')
+    main('FT Pd6f', n=100, must_print=False)
+    main('Pd6f', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd6f").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd7fm ========')
+    print(f'Initial:\n{final_probability("Pd7fm").tolist()}')
+    main('MT Pd7fm', n=100, must_print=False)
+    main('FT Pd7fm', n=100, must_print=False)
+    main('Pd7fm', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd7fm").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd6sf ========')
+    print(f'Initial:\n{final_probability("Pd6sf").tolist()}')
+    main('Pd5', n=100, must_print=False)
+    main('Pd5n', n=100, must_print=False)
+    main('Pd6sf', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd6sf").tolist()}')
+
+    CASTELLS = deepcopy(CASTELLS_CLONE)
+
+    print('======== Pd8fmp ========')
+    print(f'Initial:\n{final_probability("Pd8fmp").tolist()}')
+    main('PT Pd8fmp', n=100, must_print=False)
+    main('MT Pd8fmp', n=100, must_print=False)
+    main('FT Pd8fmp', n=100, must_print=False)
+    main('Pd8fmp', n=100, must_print=False)
+    print(f'Final:\n{final_probability("Pd8fmp").tolist()}')
