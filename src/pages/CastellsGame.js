a002e0ce751c021729a9d9763b58596e93a90818
Oriol Segura Niño <okiko22@gmail.com>
2023-08-02 03:40:59 +0200

    Not autoload outside devmode

--- src/pages/CastellsGame.js
+++ src/pages/CastellsGame.js
@@ -64,7 +64,8 @@ class CastellsGame extends Component {
 		};
 	}
 	componentDidMount() {
-		this.loadGame()
+		if (process.env.NODE_ENV === 'development')
+			this.loadGame();
 	}
 	componentDidUpdate(prevProps, prevState) {
 		this.saveGame();
@@ -300,7 +301,7 @@ class CastellsGame extends Component {
 				? <div className="flex-page">
 					<div className="btn-wrap">
 						<button className="btn" onClick={this.newGame.bind(this)} id="new-game">Nova partida</button>
-						<button className="btn disabled">Carregar partida</button>
+						<button className={`btn ${localStorage.getItem('game') ? '' : 'disabled'}`} onClick={this.loadGame.bind(this)}>Carregar partida</button>
 						<label htmlFor="import" className="btn" id="load-game">Importar partida</label>
 						<input id="import" type="file" onChange={this.loadGameFile.bind(this)} accept=".bin" style={{display: 'none'}} />
 						<p id="initial-error" className="game-error"></p>
