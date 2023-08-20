da5806dca6f9d9da4ae7632fb99aa02c6517089a
Oriol Segura Niño <okiko22@gmail.com>
2023-08-20 16:24:07 +0200

    Twitter -> X

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 		return (
 			<footer className="page-footer">
 				<div className="social-media">
-					<a href="https://twitter.com/arreplegats" className="icon twitter" target="_blank" rel="noreferrer"><img src="font-awesome/twitter.svg" alt="" /></a>
+					<a href="https://www.x.com/arreplegats" className="icon twitter-x" target="_blank" rel="noreferrer"><img src="font-awesome/twitter-x.svg" alt="" /></a>
 					<a href="https://www.instagram.com/arreplegats/" className="icon instagram" target="_blank" rel="noreferrer"><img src="font-awesome/instagram.svg" alt="" /></a>
 					<a href="https://www.youtube.com/channel/UC-RVCefwipBS8WutREwbTmw/" className="icon youtube" target="_blank" rel="noreferrer"><img src="font-awesome/youtube.svg" alt="" /></a>
 					<a href="https://www.tiktok.com/@arreplegats" className="icon tiktok" target="_blank" rel="noreferrer"><img src="font-awesome/tiktok.svg" alt="" /></a>
