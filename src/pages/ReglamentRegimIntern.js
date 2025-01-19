92aedebfa6845929709ec1c21851ab17a350c5dc
Andreu <andreuhuguet@gmail.com>
2025-01-19 12:00:01 +0100

    Typo

--- src/pages/ReglamentRegimIntern.js
+++ src/pages/ReglamentRegimIntern.js
@@ -7,7 +7,7 @@ class Estatuts extends Component {
 				<h2>Reglament de Règim Intern (2023)</h2>
 
 				<object className="fullscreen-pdf" data="/uploads/regim-intern-2023.pdf" type="application/pdf">
-					<p>No s'ha pogut mostrar el PDF. <a href="/uploads/regin-intern-2023.pdf">Descarrega'l</a>.</p>
+					<p>No s'ha pogut mostrar el PDF. <a href="/uploads/regim-intern-2023.pdf">Descarrega'l</a>.</p>
 				</object>
 			</section>
 		</>);
